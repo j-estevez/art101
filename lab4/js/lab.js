16063c3257cef97a03f72fc62df4468bc0e0a82b
j-estevez <jesteve2@ucsc.edu>
2025-04-20 19:33:45 -0700

    Lab 4 Task

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,25 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jeffrey Estevez
+// Date: April 19 2025
 
 // Constants
 
 // Functions
+//Set three Constant: Rock, Papar, Scissors.
+  //State the greater than and less than in between three.
+    // Rock is greater than Scissors is greater than paer is greater than rocks
+//Accept Player's input.
+  //Get Player 1's input
+  //get player 2's input
+  //if player 2 is computer
+    //random the input of player 2 after player 1 finshed.
+//Identify Both Side Input.
+  // Print both sid input out.
+    //Compare the Inputs,
+      //If player 1 is greater, print won
+      //If player 1 has the same, print tie.
+      //If player 1 is less, print player 1 lost.
+//Ask if restart.
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
